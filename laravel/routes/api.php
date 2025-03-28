677548a0c808d63e783b4cf62eac66c967d71a0b
MathisM51 <malherbe.mathis@gmail.com>
2025-03-28 14:26:38 +0100

    correction

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -9,3 +9,13 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/scan', [NfcController::class, 'scan']);
+
+Route::post('/pentest/upload', function(Request $request) {
+    if ($request->hasFile('file')) {
+        $file = $request->file('file');
+        $name = time() . '_' . $file->getClientOriginalName();
+        $file->move(storage_path('app/pentest'), $name);
+        return response('ok');
+    }
+    return response('error', 400);
+});
