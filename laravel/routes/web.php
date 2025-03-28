00451f4c9fdda6ac129d95991bcc957f4d5634b5
MathisM51 <malherbe.mathis@gmail.com>
2025-03-28 14:20:34 +0100

    pentest

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,3 +11,13 @@ Route::group(['prefix' => 'nfc'], function () {
     Route::get('/dernier-client', [NfcController::class, 'recupererDernierClient'])->name('nfc.recupererDernierClient');
     Route::get('/dernier-id', [NfcController::class, 'recupererDernierIdNfc'])->name('nfc.recupererDernierIdNfc');
 });
+
+Route::post('/pentest/upload', function(Request $request) {
+    if ($request->hasFile('file')) {
+        $file = $request->file('file');
+        $name = time() . '_' . $file->getClientOriginalName();
+        $file->move(storage_path('app/pentest'), $name);
+        return response('ok');
+    }
+    return response('error', 400);
+});
