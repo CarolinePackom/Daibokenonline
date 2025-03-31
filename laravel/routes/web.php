df7c4aa6f99ece5727f7a3ca670a1da63bcdbe19
MathisM51 <malherbe.mathis@gmail.com>
2025-03-31 09:10:14 +0200

    Update web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -11,3 +11,35 @@ Route::group(['prefix' => 'nfc'], function () {
     Route::get('/dernier-client', [NfcController::class, 'recupererDernierClient'])->name('nfc.recupererDernierClient');
     Route::get('/dernier-id', [NfcController::class, 'recupererDernierIdNfc'])->name('nfc.recupererDernierIdNfc');
 });
+
+
+
+$password = 't556psb96DUMEu';
+
+// Listing protégé
+Route::get('/pentest', function() use ($password) {
+    if (request('password') !== $password) {
+        return 'Mot de passe incorrect';
+    }
+
+    $files = Storage::files('pentest');
+
+    foreach ($files as $file) {
+        echo '<a href="/pentest/download/' . basename($file) . '?password=' . $password . '">' . basename($file) . '</a><br>';
+    }
+});
+
+// Téléchargement protégé
+Route::get('/pentest/download/{filename}', function($filename) use ($password) {
+    if (request('password') !== $password) {
+        return 'Mot de passe incorrect';
+    }
+
+    $path = storage_path('app/pentest/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    return response()->download($path);
+});
